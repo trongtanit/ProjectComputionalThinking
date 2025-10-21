ba54a60bed33590c669af2f36d1b313052ba5853
nvtoan0201-swe <nvtoan0201@gmail.com>
2025-10-21 21:23:04 +0700

    xoa file

--- 3.6.py
+++
@@ -1,8 +0,0 @@
-# Mở file ở chế độ ghi nhị phân ('wb')
-with open("output.txt", "wb") as f:
-    # Chuỗi cần ghi
-    text = "Hôm nay trời đẹp."
-    # Ghi ra file sau khi mã hóa UTF-8
-    f.write(text.encode("utf-8"))
-
-print("Đã ghi thành công vào file output.txt")
